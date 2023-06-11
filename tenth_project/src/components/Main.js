b429a9c62f4965490eae6bc5c19444277b39f198
Max <1998max7362@mail.ru>
2023-06-11 23:50:46 +0300

    getUserData

--- tenth_project/src/components/Main.js
+++ tenth_project/src/components/Main.js
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
+import { api } from "../utils/Api";
+
 export const Main = ({onEditProfile, onAddPlace, onEditAvatar}) => {
+  const [userData, setUserData] = useState({})
 
-
-
+  useEffect(() =>{
+    (async ()=>{
+      try{
+        const userData = await api.getUserData()
+        setUserData(userData)
+      }
+      catch (err) {
+        console.log(err)
+      }
+    })()
+  },[setUserData])
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container">
-          <img className="profile__avatar" alt="Ваша аватарка" />
+          <img src={userData.avatar} className="profile__avatar" alt="Ваша аватарка" />
           <button
             type="button"
             aria-label="edit-avatar"
@@ -17,7 +30,7 @@ export const Main = ({onEditProfile, onAddPlace, onEditAvatar}) => {
         </div>
         <div className="profile__info">
           <div className="profile__name-container">
-            <h1 className="profile__name">Жак-Ив Кусто</h1>
+            <h1 className="profile__name">{userData.name}</h1>
             <button
               type="button"
               className="profile__edit-button"
@@ -25,7 +38,7 @@ export const Main = ({onEditProfile, onAddPlace, onEditAvatar}) => {
               onClick={onEditProfile}
             ></button>
           </div>
-          <p className="profile__job">Исследователь океана</p>
+          <p className="profile__job">{userData.about}</p>
         </div>
         <button
           type="button"
